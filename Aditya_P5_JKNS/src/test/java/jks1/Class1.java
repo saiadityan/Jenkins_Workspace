94b7a21ef6983cd9d291e385c37914b67375051b
saiadityan <saiadityan18@gmail.com>
2023-01-25 09:57:56 +0530

    my new jenkins

---
+++ Aditya_P5_JKNS/src/test/java/jks1/Class1.java
@@ -0,0 +1,35 @@
+package jks1;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Class1 
+{
+	@Test
+	  public void tc004() 
+	  {
+		  Reporter.log("tc004 executed successfull", true);
+	  }
+	  @Test
+	  public void tc005() 
+	  {
+		  Reporter.log("tc005 executed successfull", true);
+	  }
+	  
+	  @Test
+	  public void tc006() 
+	  {
+		  Reporter.log("tc006 executed successfull", true);
+	  }
+	 @Test
+	  public void Logout() 
+	  {
+		  Reporter.log("Logout test successfull", true);
+	  }
+	@Test
+	  public void email() 
+	  {
+		  Reporter.log("email report sent successfull", true);
+	  }
+
+}
---
+++ Aditya_P5_JKNS/src/test/java/jks2/Class2.java
@@ -0,0 +1,31 @@
+package jks2;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Class2 
+{
+	@Test
+	  public void tc001() 
+	  {
+		  Reporter.log("tc001 executed successfull", true);
+	  }
+	  @Test
+	  public void tc002() 
+	  {
+		  Reporter.log("tc002 executed successfull", true);
+	  }
+	  
+	  @Test
+	  public void tc003() 
+	  {
+		  Reporter.log("tc003 executed successfull", true);
+	  }
+	  @Test
+	  public void Login() 
+	  {
+		  Reporter.log("Login test successfull", true);
+	  }
+
+
+}
