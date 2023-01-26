8b5956e40b19ad670ef57a42db0bc35851c0be1b
saiadityan <saiadityan18@gmail.com>
2023-01-26 06:48:23 +0530

    package 3 added

---
+++ Aditya_P5_JKNS/src/test/java/jks3/Class3.java
@@ -0,0 +1,24 @@
+package jks3;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Class3 
+{
+	  @Test
+	  public void tc007() 
+	  {
+		  Reporter.log("tc007 executed successfull", true);
+	  }
+	  @Test
+	  public void tc008() 
+	  {
+		  Reporter.log("tc008 executed successfull", true);
+	  }
+	  
+	  @Test
+	  public void tc009() 
+	  {
+		  Reporter.log("tc009 executed successfull", true);
+	  }
+}
